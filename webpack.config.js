0c1a5fe6d932fb686b20c0a09b949a46bfd21d85
Konstantin Shagan <konstantinshagan@gmail.com>
2025-01-31 20:04:16 +0300

    deleted index.html from dist, installed babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,89 @@
-/* eslint-disable global-require */
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const path = require('path');
+// const path = require('path');
 
-module.exports = {
+// module.exports = {
+//   mode: process.env.NODE_ENV || 'development',
+//   entry: './src/js/main.js',
+//   output: {
+//     filename: 'main.js',
+//     path: path.resolve(__dirname, 'dist'),
+//   },
+//   devServer: {
+//     static: path.resolve(__dirname, 'dist'),
+//     port: 8080,
+//     hot: true,
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(scss)$/,
+//         use: [
+//           {
+//             loader: 'style-loader',
+//           },
+//           {
+//             loader: 'css-loader',
+//           },
+//           {
+//             loader: 'postcss-loader',
+//             options: {
+//               postcssOptions: {
+//                 plugins: () => [
+//                   require('autoprefixer'),
+//                   new HtmlWebpackPlugin({ template: 'index.html' }),
+//                 ],
+//               },
+//             },
+//           },
+//           {
+//             loader: 'sass-loader',
+//           },
+//         ],
+//       },
+//       { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+//     ],
+//   },
+// };
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
   mode: process.env.NODE_ENV || 'development',
   entry: './src/js/main.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    port: 8080,
-    hot: true,
-  },
   module: {
     rules: [
       {
-        test: /\.(scss)$/,
-        use: [
-          {
-            loader: 'style-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
           },
-          {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: () => [
-                  require('autoprefixer'),
-                  new HtmlWebpackPlugin(),
-                ],
-              },
-            },
-          },
-          {
-            loader: 'sass-loader',
-          },
-        ],
+        },
       },
       { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  output: {
+    clean: true,
   },
 };
