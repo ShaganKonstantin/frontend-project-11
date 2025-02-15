d263a40ba5fd9a4c842e9d21e72d7a3f2a555958
Konstantin Shagan <konstantinshagan@gmail.com>
2025-02-15 12:17:01 +0300

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -41,9 +41,8 @@ export default {
     }),
   ],
   output: {
-    // filename: 'main.js',
-    // path: path.resolve(__dirname, 'dist'),
-    clean: true,
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     static: path.resolve(__dirname, 'dist'),
