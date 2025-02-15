4af3e5b75b49fd31247bb20dbc770f7b23fa2e4d
Konstantin Shagan <konstantinshagan@gmail.com>
2025-02-15 13:00:08 +0300

    text5

--- src/js/rssFetcher.js
+++ src/js/rssFetcher.js
@@ -8,17 +8,24 @@ import axios from 'axios';
 //     return response.data.contents;
 //   });
 
-const fetchRSS = (url) => axios.get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`)
-  .then((response) => {
-    if (response.status < 200 || response.status >= 300) {
-      console.error('Network error:', response.status, response.statusText);
+const fetchRSS = (url) => {
+  const urlWithDisableCache = new URL(url);
+  if (!urlWithDisableCache.searchParams.has('disableCache')) {
+    urlWithDisableCache.searchParams.append('disableCache', 'true');
+  }
+  const finalUrl = urlWithDisableCache.toString();
+
+  return axios.get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(finalUrl)}`)
+    .then((response) => {
+      if (response.status < 200 || response.status >= 300) {
+        throw new Error('networkError');
+      }
+      return response.data.contents;
+    })
+    .catch((error) => {
+      console.error('Error fetching RSS:', error);
       throw new Error('networkError');
-    }
-    return response.data.contents;
-  })
-  .catch((error) => {
-    console.error('fetchRSS error:', error);
-    throw error;
-  });
+    });
+};
 
 export default fetchRSS;
