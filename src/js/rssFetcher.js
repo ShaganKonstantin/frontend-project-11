dc6da0bb48d0f3709dc19f31935e0615dc6c9524
Konstantin Shagan <konstantinshagan@gmail.com>
2025-02-16 13:21:53 +0300

    rssFetcher->added &disableCache=true to url

--- src/js/rssFetcher.js
+++ src/js/rssFetcher.js
@@ -1,33 +1,16 @@
 import axios from 'axios';
 
-// const fetchRSS = (url) => axios.get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`)
-//   .then((response) => {
-//     if (response.status < 200 || response.status >= 300) {
-//       throw new Error('networkError');
-//     }
-//     return response.data.contents;
-//   });
-
-const fetchRSS = (url) => {
-  // Создаем новый объект URL из предоставленного URL
-  const apiUrl = new URL('https://allorigins.hexlet.app/get');
-
-  // Устанавливаем параметры для запроса
-  apiUrl.searchParams.set('url', encodeURIComponent(url));
-  apiUrl.searchParams.set('disableCache', 'true'); // Добавляем параметр disableCache
-
-  return axios.get(apiUrl.toString())
-    .then((response) => {
-      if (response.status < 200 || response.status >= 300) {
-        console.error('Сетевая ошибка:', response.status, response.statusText);
-        throw new Error('networkError');
-      }
-      return response.data.contents;
-    })
-    .catch((error) => {
-      console.error('Ошибка fetchRSS:', error);
-      throw error;
-    });
-};
+const fetchRSS = (url) => axios.get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&disableCache=true`)
+  .then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      console.error('Network error:', response.status, response.statusText);
+      throw new Error('networkError');
+    }
+    return response.data.contents;
+  })
+  .catch((error) => {
+    console.error('fetchRSS error:', error);
+    throw error;
+  });
 
 export default fetchRSS;
