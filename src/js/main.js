86415f9b214532e55c4a9dc1e2d4552b4515147a
Konstantin Shagan <konstantinshagan@gmail.com>
2025-02-27 11:28:46 +0300

    main.js -> made async/await into promise

--- src/js/main.js
+++ src/js/main.js
@@ -8,13 +8,11 @@ import resources from '../locales/locales.js';
 import fetchRSS from './rssFetcher.js';
 import parseRSS from './rssParser.js';
 
-const initI18next = async () => {
-  await i18next.init({
-    lng: 'ru',
-    fallbackLng: 'en',
-    resources,
-  });
-};
+const initI18next = () => i18next.init({
+  lng: 'ru',
+  fallbackLng: 'en',
+  resources,
+});
 
 // Инициализация yup
 const initYup = () => {
@@ -30,119 +28,124 @@ const initYup = () => {
 };
 
 // Инициализация приложения
-const initApp = async () => {
-  await initI18next();
-  initYup();
-
-  const state = {
-    form: {
-      error: null,
-    },
-    feeds: [],
-    posts: [],
-    urls: [],
-    readPosts: new Set(),
-    message: null,
-  };
-
-  const watchedState = initView(state);
-
-  const urlSchema = yup.string().url().required();
-
-  const validateUrl = (url) => urlSchema
-    .validate(url)
+const initApp = () => {
+  initI18next()
     .then(() => {
-      watchedState.form.error = null;
-      return url;
-    })
-    .catch(() => {
-      watchedState.form.error = i18next.t('invalidURL');
-      return null;
-    });
+      initYup();
 
-  const addFeed = (feed, posts) => {
-    watchedState.feeds = [...watchedState.feeds, feed];
-    watchedState.posts = [...watchedState.posts, ...posts];
-  };
+      const state = {
+        form: {
+          error: null,
+        },
+        feeds: [],
+        posts: [],
+        urls: [],
+        readPosts: new Set(),
+        message: null,
+      };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const urlInput = document.getElementById('url-input');
-    const url = urlInput.value.trim();
-    const submitButton = document.querySelector('button[type="submit"]');
+      const watchedState = initView(state);
 
-    submitButton.disabled = true;
+      const urlSchema = yup.string().url().required();
 
-    watchedState.form.error = null;
-    state.message = null;
-
-    if (state.urls.includes(url)) {
-      watchedState.form.error = 'alreadyAdded';
-      submitButton.disabled = false;
-      return;
-    }
-
-    validateUrl(url)
-      .then((validatedUrl) => {
-        if (validatedUrl) {
-          return fetchRSS(validatedUrl);
-        }
-        throw new Error('invalidURL');
-      })
-      .then((data) => parseRSS(data))
-      .then(({ feed, posts }) => {
-        addFeed(feed, posts);
-        state.urls.push(url);
-
-        urlInput.value = '';
-        urlInput.focus();
-
-        watchedState.message = 'success';
-        setTimeout(() => {
-          watchedState.message = null;
-        }, 5000);
-      })
-      .catch((error) => {
-        state.message = error.message;
-        watchedState.form.error = error.message;
-      })
-      .finally(() => {
-        submitButton.disabled = false;
-      });
-  };
-
-  // Функция для периодического обновления постов
-  const checkForUpdates = () => {
-    state.urls.forEach((url) => {
-      fetchRSS(url)
-        .then((data) => {
-          const { posts } = parseRSS(data);
-
-          const newPosts = posts.filter(
-            (post) => !state.posts.some((existingPost) => existingPost.link === post.link),
-          );
-
-          if (newPosts.length > 0) {
-            watchedState.posts = [...state.posts, ...newPosts];
-          } else {
-            console.log(`No new posts for ${url}`);
-          }
+      const validateUrl = (url) => urlSchema
+        .validate(url)
+        .then(() => {
+          watchedState.form.error = null;
+          return url;
         })
         .catch(() => {
-          console.error(`Fetching error for ${url}`);
+          watchedState.form.error = i18next.t('invalidURL');
+          return null;
         });
+
+      const addFeed = (feed, posts) => {
+        watchedState.feeds = [...watchedState.feeds, feed];
+        watchedState.posts = [...watchedState.posts, ...posts];
+      };
+
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        const urlInput = document.getElementById('url-input');
+        const url = urlInput.value.trim();
+        const submitButton = document.querySelector('button[type="submit"]');
+
+        submitButton.disabled = true;
+
+        watchedState.form.error = null;
+        state.message = null;
+
+        if (state.urls.includes(url)) {
+          watchedState.form.error = 'alreadyAdded';
+          submitButton.disabled = false;
+          return;
+        }
+
+        validateUrl(url)
+          .then((validatedUrl) => {
+            if (validatedUrl) {
+              return fetchRSS(validatedUrl);
+            }
+            throw new Error('invalidURL');
+          })
+          .then((data) => parseRSS(data))
+          .then(({ feed, posts }) => {
+            addFeed(feed, posts);
+            state.urls.push(url);
+
+            urlInput.value = '';
+            urlInput.focus();
+
+            watchedState.message = 'success';
+            setTimeout(() => {
+              watchedState.message = null;
+            }, 5000);
+          })
+          .catch((error) => {
+            state.message = error.message;
+            watchedState.form.error = error.message;
+          })
+          .finally(() => {
+            submitButton.disabled = false;
+          });
+      };
+
+      // Функция для периодического обновления постов
+      const checkForUpdates = () => {
+        state.urls.forEach((url) => {
+          fetchRSS(url)
+            .then((data) => {
+              const { posts } = parseRSS(data);
+
+              const newPosts = posts.filter(
+                (post) => !state.posts.some((existingPost) => existingPost.link === post.link),
+              );
+
+              if (newPosts.length > 0) {
+                watchedState.posts = [...state.posts, ...newPosts];
+              } else {
+                console.log(`No new posts for ${url}`);
+              }
+            })
+            .catch(() => {
+              console.error(`Fetching error for ${url}`);
+            });
+        });
+        setTimeout(checkForUpdates, 5000);
+      };
+
+      const init = () => {
+        updateInterfaceTexts();
+        document.querySelector('.rss-form').addEventListener('submit', handleSubmit);
+
+        setTimeout(checkForUpdates, 5000);
+      };
+
+      init();
+    })
+    .catch((error) => {
+      console.error('Ошибка инициализации i18next:', error);
     });
-    setTimeout(checkForUpdates, 5000);
-  };
-
-  const init = () => {
-    updateInterfaceTexts();
-    document.querySelector('.rss-form').addEventListener('submit', handleSubmit);
-
-    setTimeout(checkForUpdates, 5000);
-  };
-
-  init();
 };
 
 // Запуск приложения
